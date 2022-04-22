3433b97878f81792f09edb5e42834d6a6328d9e1
SurajChaudhary9 <surajchaudhary0707@gmail.com>
2022-04-22 13:55:20 +0530

    Added : Calculate fare for minimum distance

--- src/main/java/com/bridgelabs/InvoiceGenerator.java
+++ src/main/java/com/bridgelabs/InvoiceGenerator.java
@@ -3,9 +3,12 @@ package com.bridgelabs;
 public class InvoiceGenerator {
     private static final double MINIMUM_COST_PER_KILOMETER =1 ;
     private static final int COST_PER_TIME =10 ;
-
+    private static final double MINIMUM_FARE =5 ;
+    //STEP 1 V2 : Minimum Fare
     public double calculateFare(double distance, int time){
-        return distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
-        
+        double totalFare = distance * MINIMUM_COST_PER_KILOMETER + time * COST_PER_TIME;
+       if(totalFare < MINIMUM_FARE)
+           return MINIMUM_FARE;
+        return totalFare;
     }
 }
--- src/test/java/com/bridgelabs/InvoiceServiceTest.java
+++ src/test/java/com/bridgelabs/InvoiceServiceTest.java
@@ -14,4 +14,13 @@ public class InvoiceServiceTest {
         Assert.assertEquals(25,fare,0.0);
 
     }
+    //STEP 1 V2 : Minimum Fare
+    @Test
+    public void givenLessDistanceOrTime_ShouldReturnMinFare(){
+        InvoiceGenerator invoiceGenerator = new InvoiceGenerator();
+        double distance =0.1;
+        int time =1;
+        double fare = invoiceGenerator.calculateFare(distance, time);
+        Assert.assertEquals(5,fare,0.0);
+    }
 }
